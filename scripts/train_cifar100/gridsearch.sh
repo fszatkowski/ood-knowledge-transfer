44b00431575535e5a85526c066bb85dfc15d757c
Filip Szatkowski <fmszatkowski@gmail.com>
2024-06-28 15:19:28 +0200

    Add gridsearch scripts

---
+++ scripts/train_cifar100/gridsearch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+batch_sizes=(128 256)
+lrs=(0.1 0.01)
+wds=(0.0005 0.0)
+decays=(0.1 0.2)
+seeds=(0 1 2)
+
+for seed in "${seeds[@]}"; do
+  for batch_size in "${batch_sizes[@]}"; do
+    for lr in "${lrs[@]}"; do
+      for wd in "${wds[@]}"; do
+        for decay in "${decays[@]}"; do
+          sbatch -A plgimprmoe-gpu-a100 -p plgrid-gpu-a100 scripts/train_cifar100/slurm_script.sh $batch_size $lr $wd $decay $seed
+        done
+      done
+    done
+  done
+done
+
---
+++ scripts/train_cifar100/slurm_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --time=48:00:00   # walltime
+#SBATCH --ntasks-per-node=4   # number of processor cores (i.e. tasks)
+#SBATCH --gpus=1
+
+set -e
+
+eval "$(conda shell.bash hook)"
+conda activate ood_kd
+
+batch_size=$1
+lr=$2
+wd=$3
+decay=$4
+seed=$5
+
+python src/main.py \
+  --cfg_path configs/train/cifar100_resnet18.yaml \
+  batch_size_train=${batch_size} \
+  learning_rate=${lr} \
+  weight_decay=${wd} \
+  lr_decay=${decay} \
+  seed=${seed} \
+  save_dir=pretrained/cifar100/resnet18_bs_${batch_size}_lr_${lr}_wd_${wd}_lr_decay_${decay}_seed_${seed}/last.ckpt \
+  exp_name=train_cifar100_resnet18_bs_${batch_size}_lr_${lr}_wd_${wd}_lr_decay_${decay}
